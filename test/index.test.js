8732db316765a43a6a832249d0f2b4f69af68a19
Raul Sanchez <raulsf66@gmail.com>
2019-09-29 13:45:36 +0200

    Issue #3: Arreglado test básico

--- test/index.test.js
+++ test/index.test.js
@@ -4,10 +4,11 @@ var server = require('../bin/www');
 
 describe('Index', function() {
   describe('/GET', function() {
-    it('should return 200', function() {
+    it('should return 200', function(done) {
         request.get("http://localhost:3000", (err, res, bod) => {
             assert.equal(res.statusCode, 200);
-        });    
+            done();
+        });
     });
   });
 });
