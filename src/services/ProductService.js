e0578d74dcd09976ca2a2ac3da27e0f704247984
Raul Sanchez <raulsf66@gmail.com>
2019-10-02 18:58:31 +0200

    Añadido método get

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
+const path = require('path');
+
 
 class ProductService{
     constructor(){
-
+        this.products = null;
+        this.products = JSON.parse(fs.readFileSync(path.join(__dirname,'../db/products.json')));
     }
 
     getProduct(label){
+        let prod = null
+        this.products.forEach(element => {
+            if (label === element.label){
+                prod = element;
+                return
+            }
+        });
 
+        return prod;
     }
 
     addProduct(product){
@@ -21,3 +32,7 @@ class ProductService{
 
     }
 }
+
+
+var handler = new ProductService();
+module.exports = handler;
