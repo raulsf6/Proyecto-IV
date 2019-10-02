6c79d468a89e0368585440e8e69ca6158d55c1d0
Raul Sanchez <raulsf66@gmail.com>
2019-10-02 17:16:46 +0200

    Issue #6: Añadida definición de métodos de la clase

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,3 +1,23 @@
+const fs = require('fs');
+
 class ProductService{
-    
+    constructor(){
+
+    }
+
+    getProduct(label){
+
+    }
+
+    addProduct(product){
+
+    }
+
+    updateProduct(product){
+
+    }
+
+    deleteProduct(label){
+
+    }
 }
