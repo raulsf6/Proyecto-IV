b9acfd8123640181c6eabd28ca8900da6d5f01c0
Raul Sanchez <raulsf66@gmail.com>
2019-10-03 20:46:28 +0200

    Issue #5: Modificado error handler

--- src/app.js
+++ src/app.js
@@ -16,20 +16,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404 (because is the last possible route) and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404, "route does not exist"));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // send the error
+  res.status(err.status || 500).send("Something went wrong");
 });
 
 module.exports = app;
