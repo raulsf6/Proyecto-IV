653096fc365a81dc7172fae783f45e3183eb80ba
Raul Sanchez <raulsf66@gmail.com>
2019-10-03 20:08:36 +0200

    Issue #8: Cambiada implementación para testear los métodos asíncronos. Closes #8

--- test/index.test.js
+++ test/index.test.js
@@ -5,50 +5,56 @@ var server = require('../src/bin/www');
 var prodservice = require('../src/services/ProductService');
 
 describe('Storage handler', function() {
+  var product = {
+    "label": "00003",
+    "name": "spaghetti",
+    "nutritional": {
+      "energy": 15,
+      "fat": 12,
+      "carbohydrates": 7,
+      "proteins": 16
+    },
+    "brand": "gallo",
+    "allergens": [
+    ]
+  };
+
   describe('Get object with label 00001', function() {
-    it('should return object with label 00001', function(done) {
-        var prod = prodservice.getProduct('00001');
-        expect(prod).to.exist;
-        expect(prod.label).to.equal('00001');
-        done();
+    it('should return object with label 00001', function() {
+        
+      var prod = prodservice.getProduct('00001');
+      expect(prod).to.exist;
+      expect(prod.label).to.equal('00001');
+      
+    });
+  });
+
+  describe('Set object with label 000003', function() {
+    it('should create a full object', async function() {
+
+      prod = await prodservice.addProduct(product);
+      expect(prod).to.exist;
+      prod = prodservice.getProduct('00003');
+      expect(prod).to.exist;
+
     });
   });
 
   
-  describe('Set object with label 000003', function() {
-    it('should create a full object', function(done) {
-        product = {
-          "label": "00003",
-          "nutritional": {
-            "energy": 15,
-            "fat": 12,
-            "carbohydrates": 7,
-            "proteins": 16
-          },
-          "brand": "gallo",
-          "allergens": [
-          ]
-        }
-        prod = prodservice.addProduct(product);
-        expect(prod).to.exist;
-        prod = prodservice.getProduct('00003');
-        expect(prod).to.exist;
-        done();
-    });
-  });
-
-  /*
   describe('Update object with label 000002', function() {
-    it('should update object wuth label 000002', function(done) {
-        
+    it('should update object with label 000002', async function() {
+      product.brand = "findus";
+      await prodservice.updateProduct(product);
     });
   });
-  */
+  
 
   describe('Delete object with label 000003', function() {
-    it('should delete object wuth label 000003', function(done) {
-        prodservice.deleteProduct('00003');
-        done();
+    it('should delete object with label 000003', async function() {
+        
+      await prodservice.deleteProduct('00003');
+      prod = prodservice.getProduct('00003');
+      expect(prod).not.to.exist;
     });
   });
   
