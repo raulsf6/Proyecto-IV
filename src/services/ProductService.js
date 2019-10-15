710e565dc15b962729abf8c9568c8157b51b4bef
Raul Sanchez <raulsf66@gmail.com>
2019-10-15 18:32:21 +0200

    Issue #20: añadidas excepciones y sus respectivos status

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -12,6 +12,13 @@ class ProductService{
 
     getProduct(label){
         var prod = this.db.get('prods').find({label: label}).value();
+        
+        if (prod == null){
+            var error = new Error("Product does not exist");
+            error.status = 404;
+            throw error;
+        }
+        
         return prod;
     }
 
@@ -21,15 +28,21 @@ class ProductService{
             return this.db.get('prods').push(product).write();
         }
         else{
-            throw new Error("WRITE_ERROR: Product already exists");
+            var error = new Error("Product already exists");
+            error.status = 400;
+            throw error;
         }
     }
 
     updateProduct(product){
         if(!this.db.get('prods').find({label: product.label}).isEmpty().value())
             return this.db.get('prods').find({label: product.label}).assign(product).write();
-        else
-            throw new Error("UPDATE_ERROR: Product does not exist");
+        else{
+            var error = new Error("Product does not exist");
+            error.status = 404;
+        
+            throw error;
+        }
     }
 
     deleteProduct(label){
