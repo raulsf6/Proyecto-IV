3bf661571541ca9d122a87c6b3e41590ebd1f389
Raul Sanchez <raulsf66@gmail.com>
2019-10-20 17:46:47 +0200

    Issue #5: reestructurado código

--- src/routes/prods.js
+++ src/routes/prods.js
@@ -5,49 +5,53 @@ var ProductService = require('../services/ProductService');
 var router = express.Router();
 var prodService = new ProductService(db)
 
-// POST a product.
-router.post('/prods', function(req, res, next) {
-    var prod = req.body;
-    try {
-        prodService.addProduct(prod);
-    } catch (error) {
-        next(error);
-    }
-    res.status(200).send(prod);
-    return;
-});
 
 router.get('/prods/:label', function(req, res, next) {
     var label = req.params.label;
+    
     try {
         var prod = prodService.getProduct(label);
     } catch (error) {
         next(error);
         return;
     }
+    
+    res.status(200).contentType('application/json').send(prod);
+});
 
-    res.status(200).send(prod);
-    return;
+router.post('/prods', async function(req, res, next) {
+    var prod = req.body;
+    
+    try {
+        await prodService.addProduct(prod);
+    } catch (error) {
+        next(error);
+        return;
+    }
+    
+    res.status(200).contentType('application/json').send(prod);
 });
 
 router.put('/prods/:label', function(req, res, next){
     var label = req.params.label;
     var prod = req.body;
+    
     try {
         prodService.updateProduct(prod);
     } catch (error) {
         next(error);
         return;
     }
-    res.status(200).send(prod);
+    
+    res.status(200).contentType('application/json').send(prod);
 });
 
 router.delete('/prods/:label', function(req, res, next){
     var label = req.params.label;
     prodService.deleteProduct(label);
-    res.status(200).send("Deleted object");
+    
+    res.status(200).contentType('application/json').send({"message": "Deleted object"});
 });
   
-
 
 module.exports = router;
