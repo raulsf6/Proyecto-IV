ab8eea25f26ad205008ce9427fb5beb467a49b21
Raul Sanchez <raulsf66@gmail.com>
2019-11-20 19:05:49 +0100

    Issue #20: Añadidos tests para el manejo de errores

--- test/rest.test.js
+++ test/rest.test.js
@@ -3,21 +3,50 @@ var expect = require('chai').expect;
 var app = require('../src/app');
 var supertest = require('supertest');
 
-describe('REST API', function () {
+var prod = {
+    "label": "00003",
+    "name": "spaghetti",
+    "nutritional": {
+      "energy": 15,
+      "fat": 12,
+      "carbohydrates": 7,
+      "proteins": 16
+    },
+    "brand": "gallo",
+    "allergens": [
+    ]
+};
 
-    var prod = {
-        "label": "00003",
-        "name": "spaghetti",
-        "nutritional": {
-          "energy": 15,
-          "fat": 12,
-          "carbohydrates": 7,
-          "proteins": 16
-        },
-        "brand": "gallo",
-        "allergens": [
-        ]
-    }
+var prod2 = {
+    "label": "1111",
+    "name": "spaghetti",
+    "nutritional": {
+      "energy": 15,
+      "fat": 12,
+      "carbohydrates": 7,
+      "proteins": 16
+    },
+    "brand": "gallo",
+    "allergens": [
+    ]
+};
+
+var prod3 = {
+    "label": "00001",
+    "name": "spaghetti",
+    "nutritional": {
+      "energy": 15,
+      "fat": 12,
+      "carbohydrates": 7,
+      "proteins": 16
+    },
+    "brand": "gallo",
+    "allergens": [
+    ]
+};
+
+describe('REST API', function () {
+    
     
     describe('GET /prods/00001', function (){
         it('Should return 200 and a json object', function (done){
@@ -57,4 +86,53 @@ describe('REST API', function () {
             .expect(200, done);
         });
     })
+});
+
+describe('Error handling', function() {
+    describe('Get product that does not exist', function(){
+        it('Should return 404', function(done){
+            supertest(app)
+            .get('/prods/1111')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(404, done);
+        });
+    });
+
+    describe('Post product that already exist', function(){
+        it('Should return 400', function(done){
+            supertest(app)
+            .post('/prods')
+            .send(prod3)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done);
+        });
+    });
+
+    describe('Update product that does not exist', function(){
+        it('Should return 404', function(done){
+            supertest(app)
+            .put('/prods/1111')
+            .send(prod2)
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(404, done);
+        });
+    });
+
+    describe('Delete product that does not exist', function(){
+        it('Should return 404', function(done){
+            supertest(app)
+            .delete('/prods/1111')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(404, done);
+        });
+    });
+
+    describe('Bad request', function(){
+        it('Should return 400', function(done){
+            supertest(app)
+            .post('/prods')
+            .expect('Content-Type', 'application/json; charset=utf-8')
+            .expect(400, done);
+        });
+    });
 });
